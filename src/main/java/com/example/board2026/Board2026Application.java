c89720b786f44a9e001db843ddb39cd5a7e0f2d2
ECs428 <4ecs28@gmail.com>
2026-04-01 23:57:41 +0900

    [add] Spring Boot Set up

---
+++ src/main/java/com/example/board2026/Board2026Application.java
@@ -0,0 +1,13 @@
+package com.example.board2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board2026Application.class, args);
+	}
+
+}
