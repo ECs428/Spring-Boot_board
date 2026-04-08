3b7639e84ddd777de45e11766d07d3f2271f90e8
ECs428 <4ecs28@gmail.com>
2026-04-09 00:05:29 +0900

    [add]: BoardController
    save 주소

---
+++ src/main/java/com/example/board2026/Controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.example.board2026.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board") // "/board"로 시작하는 주소를 컨트롤러가 먼저 읽음
+public class BoardController {
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+}
