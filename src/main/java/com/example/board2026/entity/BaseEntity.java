414e18c7c30c566eef21960403609e4a3ac77df0
ECs428 <4ecs28@gmail.com>
2026-04-12 23:47:34 +0900

    [add]: Entity 추가

---
+++ src/main/java/com/example/board2026/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.board2026.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    // 생성 되었을 때 시간
+    @CreationTimestamp
+    @Column(updatable = false) // 수정 시 관여 X
+    private LocalDateTime createdTime;
+
+    // 업데이트 되었을 때 시간
+    @UpdateTimestamp
+    @Column(insertable = false) // 입력 시 관여 X
+    private LocalDateTime updatedTime;
+}
+
---
+++ src/main/java/com/example/board2026/entity/BoardEntity.java
@@ -0,0 +1,34 @@
+package com.example.board2026.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// DB의 테이블 역할을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity {
+    @Id // pk 컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment (mysql 기준)
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 크기 20, not null
+    private String boardWriter;
+
+    @Column // 크기 255, null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    @Column
+    private int fileAttached; // 1 or 0
+}
