ecfbb3e872951f2d62145eb9d0dd447a3fb5e4a6
ECs428 <4ecs28@gmail.com>
2026-04-23 00:45:22 +0900

    게시글 수정

--- src/main/java/com/example/board2026/Controller/BoardController.java
+++ src/main/java/com/example/board2026/Controller/BoardController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor // 필요한 필드만 포함해서 생성자를 자동으로 만들어주는 Lombok 어노테이션
 @RequestMapping("/board") // "/board"로 시작하는 주소를 컨트롤러가 먼저 읽음
-public class  BoardController {
+public class BoardController {
     private final BoardService boardService; // 의존성 주입 (의존성 = 한 객체가 다른 객체를 필요로 하는 관계)
 
     @GetMapping("/save")
@@ -21,6 +21,7 @@ public class  BoardController {
     }
 
     @PostMapping("/save")
+//    @ModelAttribute > form(사용자가 입력한 값들을 서버로 보내는 “묶음(전송 방식)”)을 객체로 받을 때
     public String save(@ModelAttribute BoardDTO boardDTO) {
         System.out.println("boardDTP = " + boardDTO);
         boardService.save(boardDTO);
@@ -37,6 +38,7 @@ public class  BoardController {
 
     @GetMapping("/{id}")
     public String findById(@PathVariable Long id, Model model) {
+        // @PathVariable > URL 경로에서 값 꺼내는 것
         /*
             해당 게시글의 조회수를 하나 올리고
             게시글 데이터를 가져와서 detail.html에 출력
@@ -45,5 +47,21 @@ public class  BoardController {
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
         return "detail";
+    }
+
+    @GetMapping("update/{id}")
+    public String updateForm(@PathVariable Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("boardUpdate", boardDTO);
+//        th:value="${boardUpdate.id} > thymeleaf 속성 > update.html
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        BoardDTO board = boardService.update(boardDTO);
+        model.addAttribute("board", board);
+        return "detail";
+//        return "redirect:/board/" + boardDTO.getId();
     }
 }
